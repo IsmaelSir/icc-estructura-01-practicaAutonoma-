fc2ed000b3213d179b089893132a8a8f002ea874
IsmaelSir <https://github.com/IsmaelSir/ICC-EST-01-PRACTICAJAVA.git>
2024-10-07 12:38:00 -0600

    Falta los tres metodos de insercion

--- src/MetodosOrdenamiento.java
+++ src/MetodosOrdenamiento.java
@@ -3,7 +3,10 @@ import java.util.Arrays;
 public class MetodosOrdenamiento {
 
     // Método de burbuja tradicional con errores
-    // Error encontrado:
+    //
+    // Error encontrado: El metodo retorno esta devolviendo un arreglo vacio (return new int[] {})
+    // en ves del arreglo ordenado
+    // Correccion: retornamos el arreglo ordenado (return arreglo;)
     public int[] burbujaTradicional(int[] arregloOriginal) {
         int[] arreglo = Arrays.copyOf(arregloOriginal, arregloOriginal.length);
 
@@ -18,19 +21,21 @@ public class MetodosOrdenamiento {
                 }
             }
         }
-        return new int[] {};
+        return arreglo;
     }
 
     // Método de burbuja tradicional con errores
-    // Error encontrado:
-
+    //
+    // Error encontrado: el metodo if esta comparando ( arreglo[i] < arreglo[j] ) y ordenara el arreglo de mayor a menor 
+    //
+    // Correccion: Cambiamos la comparacion de "<" a ">" para que ordene de menor a mayor ( arreglo[i] > arreglo[j] )
     public int[] burbujaTradicionalSegundo(int[] arregloOriginal) {
         int[] arreglo = Arrays.copyOf(arregloOriginal, arregloOriginal.length);
 
         int n = arreglo.length;
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
-                if (arreglo[i] < arreglo[j]) {
+                if (arreglo[i] > arreglo[j]) {
                     // Intercambio de elementos
                     // Estas 3 lineas NO DEBEN ser modificadas
                     int temp = arreglo[i];
@@ -45,14 +50,15 @@ public class MetodosOrdenamiento {
     }
 
     // Método de burbuja tradicional con errores
-    // Error encontrado:
+    // Error encontrado: El indice j esta fuera del rango del arreglo ( for (int j = 0; j < n; j++) )
+    // Correccion: Poner al indice j dentro del rango del arreglo ( for (int j = 0; j < n - 1; j++) )
     public int[] burbujaTradicionalTercero(int[] arregloOriginal) {
 
         int[] arreglo = Arrays.copyOf(arregloOriginal, arregloOriginal.length);
 
         int n = arreglo.length;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n; j++) {
+        for (int i = 0; i < n- 1; i++) {
+            for (int j = 0; j < n - 1; j++) {
                 if (arreglo[j] > arreglo[j + 1]) {
                     // Intercambio de elementos
                     int temp = arreglo[j];
@@ -65,7 +71,8 @@ public class MetodosOrdenamiento {
     }
 
     // Método de selección con errores
-    // Error encontrado:
+    // Error encontrado: El metodo de seleccionPrimero esta recibiendo un arreglo pero no esta devolviendo el arreglo 
+    // Correcion: retornar el arreglo ordenado " return arreglo; "
     public int[] seleccionPrimero(int[] arregloOriginal) {
 
         int[] arreglo = Arrays.copyOf(arregloOriginal, arregloOriginal.length);
@@ -81,11 +88,13 @@ public class MetodosOrdenamiento {
             arreglo[indiceMinimo] = arreglo[i];
             arreglo[i] = smallerNumber;
         }
-
+        return arreglo;
     }
 
     // Método de selección con errores
-    // Error encontrado:
+    // Error encontrado: el segundo bucle for; el indice "j" esta fuera de rango por un "-":
+    // "for (int j = i + 1; j < arreglo.length; j--)"
+    // Correccion: el indice "j" esta sumando "for (int j = i + 1; j < arreglo.length; j++)"
     public int[] seleccionSegundo(int[] arregloOriginal) {
 
         int[] arreglo = Arrays.copyOf(arregloOriginal, arregloOriginal.length);
@@ -93,7 +102,7 @@ public class MetodosOrdenamiento {
         for (int i = 0; i < arreglo.length; i++) {
             int indiceMinimo = i;
 
-            for (int j = i + 1; j < arreglo.length; j--) {
+            for (int j = i + 1; j < arreglo.length; j++) {
                 if (arreglo[j] < arreglo[indiceMinimo]) {
                     indiceMinimo = j;
                 }
@@ -107,12 +116,15 @@ public class MetodosOrdenamiento {
     }
 
     // Método de selección con errores
-    // Error encontrado:
+    // Error encontrado: en la linea 124 hay un "length-1" y en las lineas 134 no esta sacando el valor correcto del aux
+    // "int smallerNumber = i; "; en la linea 136 esta mal en indice "arreglo[indiceMinimo] = smallerNumber"
+    // correcion: eliminar el -1 del length; tomar el valor correcto del aux "int smallerNumber = arreglo[indiceMinimo];" y
+    // colocar el indice correcto que es i "arreglo[i] = smallerNumber;"
     public int[] seleccionTercero(int[] arregloOriginal) {
 
         int[] arreglo = Arrays.copyOf(arregloOriginal, arregloOriginal.length);
 
-        for (int i = 0; i < arreglo.length - 1; i++) {
+        for (int i = 0; i < arreglo.length ; i++) {
             int indiceMinimo = i;
 
             for (int j = i + 1; j < arreglo.length; j++) {
@@ -121,9 +133,9 @@ public class MetodosOrdenamiento {
                 }
             }
 
-            int smallerNumber = arreglo[i];
+            int smallerNumber = arreglo[indiceMinimo];
             arreglo[indiceMinimo] = arreglo[i];
-            arreglo[indiceMinimo] = smallerNumber;
+            arreglo[i] = smallerNumber;
         }
         return arreglo;
     }
@@ -137,7 +149,7 @@ public class MetodosOrdenamiento {
             int key = arreglo[j];
             int i = j - 1;
 
-            while (i > 0 && arreglo[i] < key) {
+            while (i > 0 && arreglo[i] > key) {
                 arreglo[i + 1] = arreglo[i];
                 i--;
             }
